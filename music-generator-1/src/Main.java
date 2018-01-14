94af311afaa7f1ad86122811f030dfbaa1e47ee2
Riley Boyd <contact@rileyboyd.com>
2018-01-13 22:19:45 -0500

    Think I've cleaned up the workspace now, added a Hello World to the MusicMaker class

---
+++ music-generator-1/src/Main.java
@@ -0,0 +1,7 @@
+
+public class Main {
+	public static void main(String[] args) {
+		MusicMaker test = new MusicMaker();
+		test.sayHi();
+	}
+}
---
+++ music-generator-1/src/MusicMaker.java
@@ -0,0 +1,6 @@
+
+public class MusicMaker extends Main {
+	void sayHi() {
+		System.out.print("Hello");
+	}
+}
