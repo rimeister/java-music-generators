70786d06ff0fd323227a8b0e31a8dc1321a1b049
Riley Boyd <contact@rileyboyd.com>
2018-01-13 22:32:37 -0500

    Used classes from jMusic library, added a method to MusicMaker that plays a C4 tone for a quarter_note. Demo from page 15 = working.

--- music-generator-1/src/Main.java
+++ music-generator-1/src/Main.java
@@ -1,7 +1,7 @@
 
 public class Main {
 	public static void main(String[] args) {
-		MusicMaker test = new MusicMaker();
-		test.sayHi();
+		MusicMaker musicObject = new MusicMaker();
+		musicObject.playMusic();
 	}
 }
--- music-generator-1/src/MusicMaker.java
+++ music-generator-1/src/MusicMaker.java
@@ -1,6 +1,13 @@
 
-public class MusicMaker extends Main {
-	void sayHi() {
-		System.out.print("Hello");
+import jm.util.Play;
+import jm.music.data.*;
+import jm.JMC;
+
+public class MusicMaker implements JMC {
+	Note n = new Note(C4,QUARTER_NOTE);
+	
+	public void playMusic() {
+		Play.midi(n);
 	}
+	
 }
