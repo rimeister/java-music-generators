09e3fc20922fe871ff265e4ea804599b75ab5a3e
Riley Boyd <contact@rileyboyd.com>
2018-01-14 20:10:29 -0500

    Followed example from page 21, println's a cool waving line that visualizes the wave form

--- music-generator-1/src/ReadAudio.java
+++ music-generator-1/src/ReadAudio.java
@@ -3,10 +3,18 @@ import jm.util.*;
 public final class ReadAudio {
 
 	public static void main(String[] args) {
-		float[] data = Read.audio("res/harpeggio.wav");
-		for (int i = 0; i < data.length; i++) {
-			System.out.print(data[i] + "\n");
+		float[] data = Read.audio("res/harpeggio.wav"); // Put the sample data into a floating point array
+		
+		for (int i = 0; i < data.length; i+=10) {
+
+			for (int j = 0; j-40<data[i]*40;j++) {
+				System.out.print(" ");
+			}
+
+			System.out.println("o");
+			
 		}
+		
 	}
 	
 }
